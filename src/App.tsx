ccbe2b2bd63a600472ba128dfae66efc5d9756af
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-11-15 15:55:35 -0300

    feat: basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <h1>Hi</h1>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   )
 }
 
